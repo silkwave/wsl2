e0fc4c2ebbcf442dcbb29b02f23a2e9b06c60563
silkwave <silkwave24@gmail.com>
2024-12-29 20:53:17 +0900

    MONGOPDB

--- oracle.sh
+++ oracle.sh
@@ -1,3 +1,15 @@
+mkdir -p /u01/app/oracle/oradata
+chmod 777 /u01/app/oracle/oradata
+
+docker run -d --name oracle19db \
+-p 1521:1521 \
+-e ORACLE_SID=MONGO \
+-e ORACLE_PDB=MONGOPDB \
+-e ORACLE_PWD=Oracle123 \
+-v /u01/app/oracle/oradata:/opt/oracle/oradata \
+banglamon/oracle193db:19.3.0-ee
+
+
 mkdir -p ${HOME}/oradata/
 chmod 777 ${HOME}/oradata
 
@@ -31,7 +43,14 @@ podman stop oracle19c-ko
 
 podman exec -it oracle19c-ko /bin/bash
 sqlplus / as sysdba
+sqlplus '/as sysdba'
+select name from v$database;
+show con_name
+show pdbs
 
+create user docker identified by "docker123";
+grant connect, resource to docker;
+alter user docker quota unlimited on users;
 
 CREATE USER silkwave IDENTIFIED BY oraclepassword;
 GRANT ALL PRIVILEGES TO silkwave;
